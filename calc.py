31eaef3c67646f46c72b37caa9a2544188ae649e
soundaryamp09 <soundaryamp09@gmail.com>
2026-03-10 10:13:29 +0530

    my first page

---
+++ calc.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def main():
+    print("Add:", add(5, 3))
+    print("Subtract:", subtract(5, 3))
+
+if __name__ == "__main__":
+    main()
